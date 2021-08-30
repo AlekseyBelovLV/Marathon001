d80e32cc481e47a151aabfce77f8c24f6ec1e228
Aleksey Belov <gal23@inbox.lv>
2021-08-30 19:34:33 +0300

    added days 5

--- main/java/day5/Task1.java
+++ main/java/day5/Task1.java
@@ -2,6 +2,31 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car1 = new Car();
+        car1.setModel("WV Гольф");
+        car1.setColor("зелёный");
+        car1.setYearOfRelease(2021);
 
+        car1.getInfoForCar();
+    }
+}
+class Car {
+    private String model;
+    private String color;
+    int yearOfRelease;
+
+    public void setModel(String carModel) { model = carModel; }
+    public String getModel() { return model; }
+
+    public void setColor(String carColor) { color = carColor; }
+    public String getColor() { return color; }
+
+    public void setYearOfRelease(int carYearOfRelease) { yearOfRelease = carYearOfRelease;}
+    public int getYearOfRelease() { return yearOfRelease; }
+
+    void getInfoForCar() {
+        System.out.println("Модель машины: " + getModel());
+        System.out.println("Цвет машины: " + getColor());
+        System.out.println("Год выпаска: " + getYearOfRelease() + " год.");
     }
 }
--- main/java/day5/Task2.java
+++ main/java/day5/Task2.java
@@ -3,6 +3,22 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike motorbike1 = new Motorbike("Vulkan", "ораньжевый", 2020);
+        motorbike1.getInfoInMotorbike();
+    }
+}
+class Motorbike {
+    private String model;
+    private String color;
+    private int yearOfRelease;
 
+    public Motorbike(String model, String color, int yearOfRelease) {
+        this.model = model;
+        this.color = color;
+        this.yearOfRelease = yearOfRelease;
+    }
+
+    void getInfoInMotorbike() {
+        System.out.println(model + ", " + color + ", " + yearOfRelease);
     }
 }
