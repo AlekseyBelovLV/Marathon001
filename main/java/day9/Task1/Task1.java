9e1749384e8bc6c3b7c82874cdf2e8d6574524c1
Aleksey Belov <gal23@inbox.lv>
2021-09-07 10:40:39 +0300

    added days 9

--- main/java/day9/Task1/Task1.java
+++ main/java/day9/Task1/Task1.java
@@ -2,6 +2,54 @@ package day9.Task1;
 
 public class Task1 {
     public static void main(String[] args) {
+        Student student1 = new Student("Андрей", "1-й курс");
+        Teacher teacher1 = new Teacher("Анна Петровна", "Химия");
+        System.out.println(student1.getGroupName());
+        System.out.println(teacher1.getSubjectName());
+        student1.printInfo();
+        teacher1.printInfo();
+    }
+}
 
+class Human {
+    private String name;
+
+    Human(String name) { this.name = name; }
+
+    public String getName() { return name; }
+
+    public void printInfo() { System.out.println("Этот человек с именем " + name); }
+}
+
+class Teacher extends Human {
+    private String subjectName;
+
+    Teacher(String name, String subjectName) {
+        super(name);
+        this.subjectName = subjectName;
+    }
+
+    String getSubjectName() { return subjectName; }
+
+    public void printInfo() {
+        super.printInfo();
+        System.out.println("Этот преподаватель с именем " + getName());
+    }
+
+}
+
+class Student extends Human{
+    private String groupName;
+
+    Student(String name, String groupName) {
+        super(name);
+        this.groupName = groupName;
+    }
+
+    public String getGroupName() { return groupName; }
+
+    public void printInfo() {
+        super.printInfo();
+        System.out.println("Это студент с именем " + getName());
     }
 }
--- main/java/day9/Task2/TestFigures.java
+++ main/java/day9/Task2/TestFigures.java
@@ -2,6 +2,97 @@ package day9.Task2;
 
 public class TestFigures {
     public static void main(String[] args) {
+        Figure[] figures = {
+                new Triangle("Red", 10, 10, 10),
+                new Triangle("Green", 10, 20, 30),
+                new Triangle("Red", 10, 20, 15),
+                new Rectangle("Red", 5, 10),
+                new Rectangle("Orange", 40, 15),
+                new Circle("Red", 4),
+                new Circle("Red", 10),
+                new Circle("Blue", 5)
+        };
+
+        System.out.println(calculateRedPerimeter(figures));
+        System.out.println(calculateRedArea(figures));
+    }
+
+    public static double calculateRedPerimeter(Figure[] figures) {
+        String emptyColor = "Red";
+        double sumPerimeter = 0;
+        for(Figure i: figures) {
+            if(i.getColor().equals(emptyColor)) {
+                sumPerimeter += i.perimeter();
+            }
+        }
+        return sumPerimeter;
+    }
+
+    public static double calculateRedArea(Figure[] f) {
+        String emptyColor = "Red";
+        double sumArea = 0;
+        for(Figure i: f) {
+            if(i.getColor().equals(emptyColor)) {
+                sumArea += i.area();
+            }
+        }
+        return sumArea;
 
     }
+
+}
+abstract class Figure {
+    private final String color;
+
+    Figure(String color) { this.color = color; }
+
+    String getColor() { return color; }
+
+    public abstract double area();
+    public abstract double perimeter();
+}
+
+class Circle extends Figure {
+    private final int radius;
+
+    Circle(String color, int radius) {
+        super(color);
+        this.radius = radius;
+    }
+
+    public double area() { return Math.PI * (radius * radius); }
+
+    public double perimeter() { return 2 * (Math.PI * radius);}
+}
+
+class Rectangle extends Figure {
+    private final int height;
+    private final int width;
+
+    Rectangle(String color, int height, int width) {
+        super(color);
+        this.height = height;
+        this.width = width;
+    }
+
+    public double area() { return height * width;}
+
+    public double perimeter() { return 2 * (height + width);}
+}
+
+class Triangle extends Figure {
+    private final int a;
+    private final int b;
+    private final int c;
+
+    Triangle(String color, int a, int b, int c) {
+        super(color);
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double area() { return Math.sqrt(perimeter() * (perimeter() - a) * (perimeter() - b) * (perimeter() - c)); }
+
+    public double perimeter() { return a + b + c;}
 }
