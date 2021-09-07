a15c7dbd5fc34b3b000647ab1fcbf05bc33439b7
Aleksey Belov <gal23@inbox.lv>
2021-09-07 09:45:05 +0300

    added days 8

--- main/java/day8/Task1.java
+++ main/java/day8/Task1.java
@@ -2,6 +2,21 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
 
+        String str = new String();   // 2385 ms
+        for (int i = 0; i <= 20000; i++) {
+            str += (i + " ");
+        }
+        System.out.println(str);
+
+//        StringBuilder sb = new StringBuilder();  // 10 ms
+//        for (int i = 0; i <= 2000; i++) {
+//            sb.append(" ").append(i);
+//        }
+//        System.out.println(sb.toString());
+
+        long stopTime = System.currentTimeMillis();
+        System.out.println("Длительность работы в мс.: " + (stopTime - startTime));
     }
 }
--- main/java/day8/Task2.java
+++ main/java/day8/Task2.java
@@ -2,6 +2,26 @@ package day8;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        Airplane airplane1 = new Airplane("CESSNA 172", 2019, 8, 736);
+        System.out.println(airplane1);
     }
 }
+class Airplane {
+    private String producer;
+    private int year;
+    private int length;
+    private int weight;
+    private int fuel = 0;
+
+    public Airplane(String producer, int year, int length, int weight) {
+        this.producer = producer;
+        this.year = year;
+        this.length = length;
+        this.weight = weight;
+    }
+
+    public String toString() {
+        return producer + ", " + year + ", " + length + ", " + weight + ".";
+    }
+}
+
