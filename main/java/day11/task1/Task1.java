b119228427e01c5bff68d6a70e797ad55203a5a6
Aleksey Belov <gal23@inbox.lv>
2021-09-11 21:24:23 +0300

    added days 9

--- main/java/day11/task1/Task1.java
+++ main/java/day11/task1/Task1.java
@@ -1,7 +1,143 @@
 package day11.task1;
 
 public class Task1 {
+
     public static void main(String[] args) {
+        Warehouse warehouse1 = new Warehouse();
+        Picker picker1 = new Picker(warehouse1);
+        Courier courier1 = new Courier(warehouse1);
+
+        Warehouse warehouse2 = new Warehouse();
+        Picker picker2 = new Picker(warehouse2);
+        Courier courier2 = new Courier(warehouse2);
+
+        businessProcess(picker1);
+        businessProcess(courier1);
+        warehouse1.infoWarehouse();
+        picker1.infoPicker(); courier1.infoCourier();
+
+        picker2.doWork(); courier2.doWork();
+        warehouse2.infoWarehouse();
+        picker2.infoPicker(); courier2.infoCourier();
+
+        warehouse1.infoWarehouse();
+        picker1.infoPicker(); courier1.infoCourier();
 
     }
+
+    static void businessProcess(Worker worker) {
+        for(int i = 0; i < 10000; i++) {
+            worker.doWork();
+        }
+        worker.bonus();
+    }
+
+    private static void brigadePickersWork(Picker[] pickers) {
+        for(Picker i: pickers) { i.doWork();}
+    }
+    private static void infoBrigadePickers(Picker[] pickers) {
+        for(Picker i: pickers) { i.infoPicker();}
+    }
+    private static void bonusBrigadePickers(Picker[] pickers) {
+        for(Picker i: pickers) { i.bonus();}
+    }
+
+}
+
+class Warehouse {
+    private int countPickedOrders;
+    private int countDeliveredOrders;
+
+    void setCountPickedOrders(int picked) { this.countPickedOrders = picked; }
+    void setCountDeliveredOrders(int delivered) { this.countDeliveredOrders = delivered; }
+    int getCountPickedOrders() { return countPickedOrders; }
+    int getCountDeliveredOrders() { return countDeliveredOrders; }
+
+    public void infoWarehouse() {
+        String picked = Integer.toString(countPickedOrders);
+        String delivered = Integer.toString(countDeliveredOrders);
+        System.out.println("Собранные посылки: " + picked + ". Доставленные посылки: " + delivered);
+    }
+}
+
+class Picker implements Worker {
+    private int salary;
+    private int isPayed;
+    private final int pickedCap = 70000;
+    private final int BONUS = 10000;
+    Warehouse warehouse;
+
+    Picker(Warehouse w) { this.warehouse = w; }
+
+    int getSalary() { return salary; }
+    int getIsPayed() { return isPayed; }
+    int getBonus() { return BONUS; }
+
+    public void doWork() {
+        salary += 80;
+        warehouse.setCountPickedOrders( warehouse.getCountPickedOrders() + 1);
+    }
+
+    public void bonus() {
+        if(warehouse.getCountPickedOrders() >= BONUS) {
+            if((warehouse.getCountPickedOrders() / BONUS) != (isPayed / pickedCap)) {
+                isPayed += pickedCap;
+            } else {
+                System.out.println("За " + (warehouse.getCountPickedOrders() / BONUS) + " норму бонус уже был выплачен.");
+            }
+
+        } else {
+            System.out.println("Для сборщиков бонус пока не доступен.");
+        }
+    }
+
+    public void infoPicker() {
+        String salaryPicker = Integer.toString(salary);
+        String isPayedPicker = Integer.toString(isPayed);
+        System.out.println("Зарплата сборщика: " + salaryPicker + ". Бонус сборщика: " + isPayedPicker);
+    }
+}
+
+class Courier implements Worker {
+    private int salary;
+    private int isPayed;
+    private final int pickedCap = 50000;
+    private final int BONUS = 10000;
+
+    Warehouse warehouse;
+
+    Courier(Warehouse w) { this.warehouse = w; }
+
+    int getSalary() { return salary; }
+    int getIsPayed() { return isPayed; }
+    int getBonus() { return BONUS; }
+
+    public void doWork() {
+        if(warehouse.getCountPickedOrders() > warehouse.getCountDeliveredOrders()) {
+            salary += 100;
+            warehouse.setCountDeliveredOrders(warehouse.getCountDeliveredOrders() + 1);
+        } else {
+            System.out.println("Посылки ещё не собрали.");
+        }
+    }
+
+    public void bonus() {
+        if(warehouse.getCountDeliveredOrders() >= BONUS) {
+            if((warehouse.getCountDeliveredOrders() / BONUS) != (isPayed / pickedCap)) {
+                isPayed += pickedCap;
+            } else {
+                System.out.println("За " + (warehouse.getCountDeliveredOrders() / BONUS) + " норму бонус уже был выплачен.");
+            }
+
+        } else {
+            System.out.println("Для курьеров бонус пока не доступен.");
+        }
+    }
+
+    public void infoCourier() {
+        String salaryCourier = Integer.toString(salary);
+        String isPayedCourier = Integer.toString(isPayed);
+        System.out.println("Зарплата курьера: " + salaryCourier + ". Бонус курьера: " + isPayedCourier);
+    }
+
 }
--- main/java/day11/task2/Task2.java
+++ main/java/day11/task2/Task2.java
@@ -2,6 +2,316 @@ package day11.task2;
 
 public class Task2 {
     public static void main(String[] args) {
+        Warrior war = new Warrior();
+        Paladin pal = new Paladin();
+        Magician mag = new Magician();
+        Shaman sham = new Shaman();
 
+        war.physicalAttack(pal);
+        pal.info();
+        pal.physicalAttack(mag);
+        mag.info();
+        sham.healTeammate(mag);
+        mag.info();
+        mag.magicalAttack(pal);
+        pal.info();
+        sham.physicalAttack(war);
+        war.info();
+        pal.healHimself();
+        pal.info();
+        for(int i =0; i < 5; i++) {
+            war.physicalAttack(mag);
+            mag.info();
+        }
     }
+}
+
+abstract class Hero {
+    private int health;
+    private int physAtt;
+    private int magicAtt;
+    private int physDef;
+    private int magicDef;
+    private int healHimself;
+    private int healTeammate;
+
+    public abstract void info();
+
+    int getHealth() { return health; }
+    int getPhysAtt() { return physAtt; }
+    int getMagicAtt() { return magicAtt; }
+    int getPhysDef() { return physDef; }
+    int getMagicDef() { return magicDef; }
+    int getHealHimself() { return healHimself; }
+    int getHealTeammate() { return healTeammate; }
+
+    void setHealth(int health) { this.health = health; }
+}
+
+class Warrior extends Hero implements PhysAttack {
+    private int health;
+    private int physAtt;
+    private int physDef;// %
+    private int magicDef;// %
+
+    Warrior() {
+        this.health = 100;
+        this.physAtt = 30;
+        this.physDef = 80;
+        this.magicDef = 0;
+    }
+
+    int getHealth() { return health; }
+    int getPhysAtt() { return physAtt; }
+    int getPhysDef() { return physDef; }
+    int getMagicDef() { return magicDef; }
+    void setHealth(int health) { this.health = health; }
+
+    public void physicalAttack(Hero h) {
+        if(h.getHealth() > 0) {
+            if(h.getPhysDef() > 0) {
+                double tempPhysDef = h.getPhysDef();
+                int damage = this.physAtt - (int) (tempPhysDef / 100 * this.physAtt);
+                if(h.getHealth() - damage <= 0) {
+                    h.setHealth(0);
+                } else {
+                    h.setHealth(h.getHealth() - damage);
+                }
+            } else {
+                if(h.getHealth() - this.physAtt <= 0) {
+                    h.setHealth(0);
+                } else {
+                    h.setHealth(h.getHealth() - this.physAtt);
+                }
+            }
+        } else {
+            System.out.println(h + " уже убит");
+        }
+    }
+    public void info() { System.out.println("Warrior health: " + health); }
+
+}
+
+class Paladin extends Hero implements PhysAttack, Healer{
+    private int health;
+    private int physAtt;
+    private int physDef; //%
+    private int magicDef; //%
+    private int healHimself;
+    private int healTeammate;
+
+    Paladin() {
+        this.health = 100;
+        this.physAtt = 15;
+        this.physDef = 50;
+        this.magicDef = 20;
+        this.healHimself = 25;
+        this.healTeammate = 10;
+    }
+
+    int getHealth() { return health; }
+    int getPhysAtt() { return physAtt; }
+    int getPhysDef() { return physDef; }
+    int getMagicDef() { return magicDef; }
+    int getHealHimself() { return healHimself; }
+    int getHealTeammate() { return healTeammate; }
+
+    void setHealth(int health) { this.health = health; }
+
+    public void physicalAttack(Hero h) {
+        if(h.getHealth() > 0) {
+            if(h.getPhysDef() > 0) {
+                double tempPhysDef = h.getPhysDef();
+                int damage = this.physAtt - (int) (tempPhysDef / 100 * this.physAtt);
+                if(h.getHealth() - damage <= 0) {
+                    h.setHealth(0);
+                } else {
+                    h.setHealth(h.getHealth() - damage);
+                }
+            } else {
+                if(h.getHealth() - this.physAtt <= 0) {
+                    h.setHealth(0);
+                } else {
+                    h.setHealth(h.getHealth() - this.physAtt);
+                }
+            }
+        } else {
+            System.out.println(h + " уже убит");
+        }
+    }
+    public void healHimself() {
+        if(health + healHimself > 100) {
+            health = 100;
+        } else {
+            setHealth(getHealth() + healHimself);
+        }
+    }
+    public void healTeammate(Hero h) {
+        if(h.getHealth() + healTeammate > 100) {
+            h.setHealth(100);
+        } else {
+            h.setHealth(h.getHealth() + healTeammate);
+        }
+    }
+    public void info() { System.out.println("Paladin health: " + health); }
+}
+
+class Magician extends Hero implements PhysAttack, MagicAttack {
+    private int health;
+    private int physAtt;
+    private int magicAtt;
+    private int physDef;
+    private int magicDef;
+
+    Magician() {
+        this.health = 100;
+        this.physAtt = 5;
+        this.magicAtt = 20;
+        this.physDef = 0;
+        this.magicDef = 80;
+    }
+
+    int getHealth() { return health; }
+    int getPhysAtt() { return physAtt; }
+    int getMagicAtt() { return magicAtt; }
+    int getPhysDef() { return physDef; }
+    int getMagicDef() { return magicDef; }
+
+    void setHealth(int health) { this.health = health; }
+
+
+    public void physicalAttack(Hero h) {
+        if(h.getHealth() > 0) {
+            if(h.getPhysDef() > 0) {
+                double tempPhysDef = h.getPhysDef();
+                int damage = this.physAtt - (int) (tempPhysDef / 100 * this.physAtt);
+                if(h.getHealth() - damage <= 0) {
+                    h.setHealth(0);
+                } else {
+                    h.setHealth(h.getHealth() - damage);
+                }
+            } else {
+                if(h.getHealth() - this.physAtt <= 0) {
+                    h.setHealth(0);
+                } else {
+                    h.setHealth(h.getHealth() - this.physAtt);
+                }
+            }
+        } else {
+            System.out.println(h + " уже убит");
+        }
+    }
+    public void magicalAttack(Hero h) {
+        if(h.getHealth() > 0) {
+            if(h.getMagicDef() > 0) {
+                double tempMagDef = h.getMagicDef();
+                int damage = this.physAtt - (int) (tempMagDef / 100 * this.magicAtt);
+                if(h.getHealth() - damage <= 0) {
+                    h.setHealth(0);
+                } else {
+                    h.setHealth(h.getHealth() - damage);
+                }
+            } else {
+                if(h.getHealth() - this.magicAtt <= 0) {
+                    h.setHealth(0);
+                } else {
+                    h.setHealth(h.getHealth() - this.magicAtt);
+                }
+            }
+        } else {
+            System.out.println(h + " уже убит");
+        }
+    }
+    public void info() { System.out.println("Magician health: " + health); }
+}
+
+class Shaman extends Hero implements PhysAttack, MagicAttack, Healer {
+    private int health;
+    private int physAtt;
+    private int magicAtt;
+    private int physDef;
+    private int magicDef;
+    private int healHimself;
+    private int healTeammate;
+
+    Shaman() {
+        this.health = 100;
+        this.physAtt = 10;
+        this.magicAtt = 15;
+        this.physDef = 20;
+        this.magicDef = 20;
+        this.healHimself = 50;
+        this.healTeammate = 30;
+    }
+
+    int getHealth() { return health; }
+    int getPhysAtt() { return physAtt; }
+    int getMagicAtt() { return magicAtt; }
+    int getPhysDef() { return physDef; }
+    int getMagicDef() { return magicDef; }
+    int getHealHimself() { return healHimself; }
+    int getHealTeammate() { return healTeammate; }
+
+    void setHealth(int health) { this.health = health; }
+
+
+    public void physicalAttack(Hero h) {
+        if(h.getHealth() > 0) {
+            if(h.getPhysDef() > 0) {
+                double tempPhysDef = h.getPhysDef();
+                int damage = this.physAtt - (int) (tempPhysDef / 100 * this.physAtt);
+                if(h.getHealth() - damage <= 0) {
+                    h.setHealth(0);
+                } else {
+                    h.setHealth(h.getHealth() - damage);
+                }
+            } else {
+                if(h.getHealth() - this.physAtt <= 0) {
+                    h.setHealth(0);
+                } else {
+                    h.setHealth(h.getHealth() - this.physAtt);
+                }
+            }
+        } else {
+            System.out.println(h + " уже убит");
+        }
+    }
+
+    public void magicalAttack(Hero h) {
+        if(h.getHealth() > 0) {
+            if(h.getMagicDef() > 0) {
+                double tempMagDef = h.getMagicDef();
+                int damage = this.physAtt - (int) (tempMagDef / 100 * this.magicAtt);
+                if(h.getHealth() - damage <= 0) {
+                    h.setHealth(0);
+                } else {
+                    h.setHealth(h.getHealth() - damage);
+                }
+            } else {
+                if(h.getHealth() - this.magicAtt <= 0) {
+                    h.setHealth(0);
+                } else {
+                    h.setHealth(h.getHealth() - this.magicAtt);
+                }
+            }
+        } else {
+            System.out.println(h + " уже убит");
+        }
+    }
+    public void healHimself() {
+        if(health + healHimself > 100) {
+            health = 100;
+        } else {
+            setHealth(getHealth() + healHimself);
+        }
+    }
+
+    public void healTeammate(Hero h) {
+        if(h.getHealth() + healTeammate > 100) {
+            h.setHealth(100);
+        } else {
+            h.setHealth(h.getHealth() + healTeammate);
+        }
+    }
+    public void info() { System.out.println("Shaman health: " + health); }
 }
