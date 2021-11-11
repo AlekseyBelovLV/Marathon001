ead8b3c10b41fe212ec98429e9f639a47f3ad4bb
Aleksey Belov <gal23@inbox.lv>
2021-11-11 13:36:32 +0200

    added 14, 15 days and edited 13 day.

--- main/java/day15/Task1.java
+++ main/java/day15/Task1.java
@@ -1,7 +1,73 @@
 package day15;
 
+import day14.ScannerException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Task1 {
+    private static Scanner scn;
     public static void main(String[] args) {
+        File file = new File("main/resources/shoes.csv");
+
+        try{
+            missingShoes(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден.");
+        } catch (NumberFormatException e) {
+            System.out.println("Неправельный формат файла.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
+
+    public static void missingShoes(File file) throws IOException {
+        if(fileIsCorrect(file)) {
+            scn = new Scanner(file);
+            FileWriter fw = new FileWriter("main/resources/missing_shoes.txt", false);
+            while (scn.hasNextLine()) {
+                String line = scn.nextLine();
+                String[] strArr = line.split(";");
+                if (Integer.parseInt(strArr[2]) == 0) {
+                    for (int i = 0; i < strArr.length; i++) {
+                        fw.write(strArr[i] + ", ");
+                    }
+                    fw.write("\n");
+                }
+            }
+            fw.flush();
+            scn.close();
+        }
+    }
+
+    private static boolean fileIsCorrect(File file) throws FileNotFoundException {
+        scn = new Scanner(file);
+        if(!scn.hasNextLine()) {
+            try {
+                throw new ScannerException();
+            } catch (ScannerException e) {
+                System.out.println("Файл пустой.");
+                return false;
+            }
+        }
+
+        while(scn.hasNextLine()) {
+            String line = scn.nextLine();
+            String[] strArr2 = line.split(";");
+            if(strArr2.length != 3) {
+                try {
+                    throw new ScannerException();
+                } catch (ScannerException e) {
+                    System.out.print("Не корректное отображение информации.");
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
 }
